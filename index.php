40563077bdc4b8b1be43a142e30bd61c8702b838
html-cloud <77795649+html-cloud@users.noreply.github.com>
2021-01-22 12:58:18 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,52 @@
 <!DOCTYPEhtml>
+<?php
+require "db.html"
+{
+
+$data = $_POST;
+if ( isset($data[signup]) )
+
+$errors = array();
+if(trim($data['Login']) == ' ')
+{
+	$errors[] = 'Enter Username!';
+}
+
+if(trim($data['Email']) == ' ')
+{
+	$errors[] = 'Enter Email!';
+}
+
+if($data['Password'] == ' ')
+{
+	$errors[] = 'Enter Password!';
+}
+
+if($data['password'] == ' ')
+{
+	$errors[] = 'The second password is not entered!';
+}
+
+if($data['Password'] != $data['password'])
+{
+	$errors[] = 'The Passwords do not pass!';
+}
+
+if(trim($data['Phonel']) == ' ')
+{
+	$errors[] = 'Enter Phone!';
+}
+
+if(empty($errors))
+
+else
+
+{
+	echo '<div style="color: red;">'.arrayshift($errors).'</div><hr>';
+}
+
+}
+?>
 <html>
     <head>
         <meta charset="utf-8">
@@ -29,7 +77,7 @@
         <input type="phone">
     </div>
     <br>
-			<button><a href="sign%20up.html">Registration</a></button>
+			<button><a href="normal.html">Registration</a></button>
      	</form>
     </body>
 </html>
